67c6a599e52b3aaf4b8535e3d1b800945c85ac3c
stephen-shopopop <94382341+stephen-shopopop@users.noreply.github.com>
2022-03-11 21:38:33 +0100

    Update ulid.ts

--- src/ulid.ts
+++ src/ulid.ts
@@ -79,7 +79,7 @@ export function monotonic (): Monotonic {
     let lastTime: number = 0
     let lastRandom: string
     
-    return function (seedTimeInMs: number): string {
+    return function (seedTimeInMs?: number): string {
 	    const seed = Number.isNaN(seedTimeInMs) ? Date.now() : seedTimeInMs
 	    
         if (seed <= lastTime) {
